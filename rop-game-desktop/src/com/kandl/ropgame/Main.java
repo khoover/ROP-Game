54320375d4d0205cfda0a3b0b24edeb3b8234d13
Ken Hoover <ken.hoover@mail.utoronto.ca>
2013-06-07 10:05:02 -0400

    Added the desktop project as a testing platform.
    Signed-off-by: Ken Hoover <ken.hoover@mail.utoronto.ca>

---
+++ rop-game-desktop/src/com/kandl/ropgame/Main.java
@@ -0,0 +1,16 @@
+package com.kandl.ropgame;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "rop-game";
+		cfg.useGL20 = false;
+		cfg.width = 1365;
+		cfg.height = 800;
+		
+		new LwjglApplication(new RopGame(), cfg);
+	}
+}
