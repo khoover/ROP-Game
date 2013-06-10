d8c0f8e4168a1a350166175107cda244654d3abe
Ken Hoover <ken.hoover@mail.utoronto.ca>
2013-06-10 15:28:58 -0400

    Added view of recipes.
    Handles displaying recipes, etc.
    
    Signed-off-by: Ken Hoover <ken.hoover@mail.utoronto.ca>

---
+++ rop-game/src/com/kandl/ropgame/ui/OrderSheet.java
@@ -0,0 +1,17 @@
+package com.kandl.ropgame.ui;
+
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+
+public class OrderSheet extends Table {
+
+	public OrderSheet() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public OrderSheet(Skin skin) {
+		super(skin);
+		// TODO Auto-generated constructor stub
+	}
+
+}
