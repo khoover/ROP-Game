1291d92910801cdc4f77b4b700ab9888fd25e7da
Ken Hoover <ken.hoover@mail.utoronto.ca>
2013-06-11 14:12:56 -0400

    Changed default resolution on desktop project.
    Signed-off-by: Ken Hoover <ken.hoover@mail.utoronto.ca>

--- rop-game-desktop/src/com/kandl/ropgame/Main.java
+++ rop-game-desktop/src/com/kandl/ropgame/Main.java
@@ -8,8 +8,8 @@ public class Main {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "rop-game";
 		cfg.useGL20 = false;
-		cfg.width = 1024;
-		cfg.height = 600;
+		cfg.width = 1280;
+		cfg.height = 800;
 		
 		new LwjglApplication(new RopGame(), cfg);
 	}
