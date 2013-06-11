d5256088fd5568e8804cd35eacbdfeb35b5a290f
Ken Hoover <ken.hoover@mail.utoronto.ca>
2013-06-11 08:27:52 -0400

    Formatting things.
    Signed-off-by: Ken Hoover <ken.hoover@mail.utoronto.ca>

--- rop-game/src/com/kandl/ropgame/Ingredient.java
+++ rop-game/src/com/kandl/ropgame/Ingredient.java
@@ -3,8 +3,6 @@ package com.kandl.ropgame;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public interface Ingredient {
-
-	public void drawSide(SpriteBatch batch, float x, float y, float width, float height);
-	
+	public void drawSide(SpriteBatch batch, float x, float y, float width, float height);	
 	public void drawTop(SpriteBatch batch, float x, float y, float width, float height);
 }
