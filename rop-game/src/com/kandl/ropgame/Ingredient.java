143741eb23701311c7ebb938b83934edf153fd59
Ken Hoover <ken.hoover@mail.utoronto.ca>
2013-06-11 14:05:46 -0400

    Added static height field.
    Useful for drawing ingredients stacked, etc.
    
    Signed-off-by: Ken Hoover <ken.hoover@mail.utoronto.ca>

--- rop-game/src/com/kandl/ropgame/Ingredient.java
+++ rop-game/src/com/kandl/ropgame/Ingredient.java
@@ -3,6 +3,8 @@ package com.kandl.ropgame;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public interface Ingredient {
+	public static int HEIGHT = 100;
+	
 	public void drawSide(SpriteBatch batch, float x, float y, float width, float height);	
 	public void drawTop(SpriteBatch batch, float x, float y, float width, float height);
 }
