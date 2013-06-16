d03a1194a7dc6fcc490d634faab2c49d37c47a55
Ken Hoover <ken.hoover@mail.utoronto.ca>
2013-06-16 16:40:21 -0400

    Added all the methods I feel TableManager needs.
    Will place tips, find tables and groups, and ensure things don't go
    crazy.
    
    Signed-off-by: Ken Hoover <ken.hoover@mail.utoronto.ca>

--- rop-game/src/com/kandl/ropgame/managers/TableManager.java
+++ rop-game/src/com/kandl/ropgame/managers/TableManager.java
@@ -1,5 +1,40 @@
 package com.kandl.ropgame.managers;
 
-public abstract class TableManager {
+import com.badlogic.gdx.utils.Array;
+import com.kandl.ropgame.model.Group;
 
+public abstract class TableManager {
+	public static final int TABLEWIDTH = 640;
+	public static final int BUTTONHEIGHT = 334;
+	
+	private static Array<Group> tables = new Array<Group>(10);
+	
+	public static int assignTable(Group g) {
+		while (true) {
+			int place = ((int) Math.random() * 10);
+			if (tables.get(place) != null) continue;
+			tables.set(place, g);
+			return place;
+		}
+	}
+	
+	public static void freeTable(int i) {
+		tables.set(i, null);
+	}
+	
+	public static void putTip(int i, double tip) {
+		// TODO actually spawn button at desired location and place tip.
+	}
+	
+	public static void freeTable(Group g) {
+		freeTable(tables.indexOf(g, true));
+	}
+	
+	public static int findTableFromGroup(Group g) {
+		return tables.indexOf(g, true);
+	}
+	
+	public static Group findGroupFromTable(int i) {
+		return tables.get(i);
+	}
 }
