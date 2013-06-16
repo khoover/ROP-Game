d9ff86aab0d8f87249408c0739589bc2b46ee077
Ken Hoover <ken.hoover@mail.utoronto.ca>
2013-06-16 16:43:48 -0400

    GroupManager now creates people, will spawn on timer.
    Signed-off-by: Ken Hoover <ken.hoover@mail.utoronto.ca>

--- rop-game/src/com/kandl/ropgame/managers/GroupManager.java
+++ rop-game/src/com/kandl/ropgame/managers/GroupManager.java
@@ -1,7 +1,33 @@
 package com.kandl.ropgame.managers;
 
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.kandl.ropgame.model.Group;
+import com.kandl.ropgame.view.Person;
 
 public class GroupManager extends Actor {
-
+	private final int MAX = 3;
+	private int current = 0;
+	private float spawnGap = 0;
+	private float despawnGap = 0;
+	
+	public void createGroup() {
+		current++;
+		spawnGap = 0;
+		Person p1 = new Person(Math.random() >= 0.5);
+		Person p2 = null;
+		if (Math.random() >= 0.5) p2 = new Person(Math.random() >= 0.5);
+		Group g = new Group(p1, p2);
+	}
+	
+	@Override
+	public void act(float delta) {
+		spawnGap += delta;
+		despawnGap += delta;
+		if (spawnGap >= 30 && despawnGap >= 10 && current < MAX) createGroup();
+	}
+	
+	public void despawn() {
+		current--;
+		despawnGap = 0;
+	}
 }
