7a717af4b5e0a3328c0f4f065ab9badcc337fc63
Ken Hoover <ken.hoover@mail.utoronto.ca>
2013-06-30 11:40:01 -0400

    Changed the game's name to Deli Rush
    Signed-off-by: Ken Hoover <ken.hoover@mail.utoronto.ca>

--- rop-game-desktop/src/com/kandl/ropgame/Main.java
+++ rop-game-desktop/src/com/kandl/ropgame/Main.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 public class Main {
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-		cfg.title = "rop-game";
+		cfg.title = "Deli Rush";
 		cfg.useGL20 = false;
 		cfg.width = 1280;
 		cfg.height = 800;
