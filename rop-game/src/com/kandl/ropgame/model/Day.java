6b26cff829b825aa9890036c42c7e27a912e6866
Ken Hoover <ken.hoover@mail.utoronto.ca>
2013-07-23 21:08:46 -0400

    Added all the days.
    Signed-off-by: Ken Hoover <ken.hoover@mail.utoronto.ca>

--- rop-game/src/com/kandl/ropgame/model/Day.java
+++ rop-game/src/com/kandl/ropgame/model/Day.java
@@ -13,14 +13,21 @@ public class Day {
 	public static final Map<Integer, Day> dayMapper = new HashMap<Integer, Day>();
 	static {
 		dayMapper.put(1, new Day(3,1,(String[]) null));
-		dayMapper.put(2, new Day(4,1,"One more person is coming today."));
-		dayMapper.put(3, new Day(5,2,"Another person is coming today.","Two groups are coming at a time, now."));
+		dayMapper.put(3, new Day(4,1,"One more person is coming today."));
+		dayMapper.put(4, new Day(5,2,"Another person is coming today.","Two groups are coming at a time, now."));
+		dayMapper.put(5, new Day(6,2,"Six people are going to visit today."));
+		dayMapper.put(6, new Day(6,3,"Three groups are coming at a time."));
+		dayMapper.put(7, new Day(7,3,"Another person is visiting today."));
+		dayMapper.put(8, new Day(8,3,"One more person is coming today."));
+		dayMapper.put(9, new Day(9,3,"Nine people are visiting today."));
+		dayMapper.put(10, new Day(10,4,"Ten people are visiting today.","Four groups are coming at a time!","Today's the last day!"));
 	}
 	
 	public Day(int dayMax, int dayConcurrent, String... dayNotes) {
 		this.dayMax = dayMax;
 		this.dayConcurrent = dayConcurrent;
-		this.dayNotes = new Array<String>(dayNotes);
+		if (dayNotes != null) { this.dayNotes = new Array<String>(dayNotes); }
+		else { this.dayNotes = new Array<String>(0); }
 	}
 	
 	public int getDayMax() {
