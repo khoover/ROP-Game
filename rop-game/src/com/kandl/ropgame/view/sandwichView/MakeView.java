be79e40801fb660b141c527551726fefe0eb065c
Ken Hoover <ken.hoover@mail.utoronto.ca>
2013-07-30 11:00:45 -0400

    Fixed complaint regarding hard-to-drop ingredients.
    Signed-off-by: Ken Hoover <ken.hoover@mail.utoronto.ca>

--- rop-game/src/com/kandl/ropgame/view/sandwichView/MakeView.java
+++ rop-game/src/com/kandl/ropgame/view/sandwichView/MakeView.java
@@ -2,7 +2,9 @@ package com.kandl.ropgame.view.sandwichView;
 
 import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.utils.Align;
 import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Scaling;
 import com.kandl.ropgame.model.Sandwich;
 import com.kandl.ropgame.ingredients.*;
 
@@ -11,6 +13,7 @@ public class MakeView extends Group {
 	private Array<Image> ingredients;
 	private int size;
 	private float height;
+	private final float buffer = 100;
 	
 	public MakeView(Sandwich s) {
 		sandwich = s;
@@ -18,7 +21,11 @@ public class MakeView extends Group {
 		ingredients.add(sandwich.getBread().getSideView());
 		addActor(ingredients.get(0));
 		ingredients.get(0).setPosition(0, 0);
-		height = ingredients.get(0).getHeight() - 5;
+		ingredients.get(0).setScaling(Scaling.none);
+		ingredients.get(0).setAlign(Align.bottom);
+		height = ingredients.get(0).getHeight() - 25;
+		ingredients.get(0).setSize(ingredients.get(0).getWidth(), ingredients.get(0).getHeight()+buffer);
+		setSize(getWidth(), height + buffer);
 	}
 	
 	public void addIngredient(Ingredient i) {
@@ -26,8 +33,11 @@ public class MakeView extends Group {
 		ingredients.add(i.getSideView());
 		addActor(ingredients.peek());
 		ingredients.peek().setPosition(0, height);
+		ingredients.peek().setScaling(Scaling.none);
+		ingredients.peek().setAlign(Align.bottom);
 		height += ingredients.peek().getHeight() - 50;
-		setSize(getWidth(), height);
+		ingredients.peek().setSize(ingredients.peek().getWidth(), ingredients.peek().getHeight() + buffer);
+		setSize(getWidth(), height + buffer);
 	}
 	
 	public boolean isFull() {
