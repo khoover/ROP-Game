f3d4fa98159548214ebb47ab0a21e2838b07ce8d
Ken Hoover <ken.hoover@mail.utoronto.ca>
2013-08-05 09:38:58 -0400

    Recipe customer info toggle added into Day class.
    Signed-off-by: Ken Hoover <ken.hoover@mail.utoronto.ca>

--- rop-game/src/com/kandl/ropgame/model/Day.java
+++ rop-game/src/com/kandl/ropgame/model/Day.java
@@ -9,23 +9,25 @@ public class Day {
 	private final int dayMax;
 	private final int dayConcurrent;
 	private final Array<String> dayNotes;
+	private final boolean showInfo;
 	
 	public static final Map<Integer, Day> dayMapper = new HashMap<Integer, Day>();
 	static {
-		dayMapper.put(1, new Day(3,1,(String[]) null));
-		dayMapper.put(3, new Day(4,1,"One more person is coming today."));
-		dayMapper.put(4, new Day(5,2,"Another person is coming today.","Two groups are coming at a time, now."));
-		dayMapper.put(5, new Day(6,2,"Six people are going to visit today."));
-		dayMapper.put(6, new Day(6,3,"Three groups are coming at a time."));
-		dayMapper.put(7, new Day(7,3,"Another person is visiting today."));
-		dayMapper.put(8, new Day(8,3,"One more person is coming today."));
-		dayMapper.put(9, new Day(9,3,"Nine people are visiting today."));
-		dayMapper.put(10, new Day(10,4,"Ten people are visiting today.","Four groups are coming at a time!","Today's the last day!"));
+		dayMapper.put(1, new Day(3,1,true,(String[]) null));
+		dayMapper.put(3, new Day(4,1,true,"One more person is coming today."));
+		dayMapper.put(4, new Day(5,2,true,"Another person is coming today.","Two groups are coming at a time, now."));
+		dayMapper.put(5, new Day(6,2,true,"Six people are going to visit today."));
+		dayMapper.put(6, new Day(6,3,false,"Three groups are coming at a time.","Visitor names won't be on recipes anymore."));
+		dayMapper.put(7, new Day(7,3,false,"Another person is visiting today."));
+		dayMapper.put(8, new Day(8,3,false,"One more person is coming today."));
+		dayMapper.put(9, new Day(9,3,false,"Nine people are visiting today."));
+		dayMapper.put(10, new Day(10,4,false,"Ten people are visiting today.","Four groups are coming at a time!","Today's the last day!"));
 	}
 	
-	public Day(int dayMax, int dayConcurrent, String... dayNotes) {
+	public Day(int dayMax, int dayConcurrent, boolean showInfo, String... dayNotes) {
 		this.dayMax = dayMax;
 		this.dayConcurrent = dayConcurrent;
+		this.showInfo = showInfo;
 		if (dayNotes != null) { this.dayNotes = new Array<String>(dayNotes); }
 		else { this.dayNotes = new Array<String>(0); }
 	}
@@ -40,5 +42,9 @@ public class Day {
 	
 	public Array<String> getDayNotes() {
 		return dayNotes;
+	}
+
+	public boolean isShowInfo() {
+		return showInfo;
 	}
 }
