3d62c1d6bddefb0daa155b1d4b73c3ba7d3ddbe9
Ken Hoover <ken.hoover@mail.utoronto.ca>
2014-04-30 16:41:07 -0400

    Added Day variation meant for testing and demos.
    Signed-off-by: Ken Hoover <ken.hoover@mail.utoronto.ca>

--- rop-game/src/com/kandl/ropgame/model/Day.java
+++ rop-game/src/com/kandl/ropgame/model/Day.java
@@ -13,7 +13,7 @@ public class Day {
 	
 	public static final Map<Integer, Day> dayMapper = new HashMap<Integer, Day>();
 	static {
-		dayMapper.put(1, new Day(3,1,true,(String[]) null));
+		/*dayMapper.put(1, new Day(3,1,true,(String[]) null));
 		dayMapper.put(3, new Day(4,1,true,"One more person is coming today."));
 		dayMapper.put(4, new Day(5,2,true,"Another person is coming today.","Two groups are coming at a time, now."));
 		dayMapper.put(5, new Day(6,2,true,"Six people are going to visit today."));
@@ -21,7 +21,14 @@ public class Day {
 		dayMapper.put(7, new Day(7,3,false,"Another person is visiting today."));
 		dayMapper.put(8, new Day(8,3,false,"One more person is coming today."));
 		dayMapper.put(9, new Day(9,3,false,"Nine people are visiting today."));
-		dayMapper.put(10, new Day(10,4,false,"Ten people are visiting today.","Four groups are coming at a time!","Today's the last day!"));
+		dayMapper.put(10, new Day(10,4,false,"Ten people are visiting today.","Four groups are coming at a time!","Today's the last day!"));*/
+		
+		// This is for quick demos
+		dayMapper.put(1, new Day(4,2,true,(String[]) null));
+		dayMapper.put(2, new Day(5,2,true,"One more person is coming today."));
+		dayMapper.put(3, new Day(6,3,true,"Another person is coming today.","Three groups are coming at a time."));
+		dayMapper.put(4, new Day(8,4,true,"Two more people are coming today.","Four groups are arriving at the same time."));
+		dayMapper.put(5, new Day(10,4,false,"Ten people are going to visit today.","The names of people won't be on recipes."));
 	}
 	
 	public Day(int dayMax, int dayConcurrent, boolean showInfo, String... dayNotes) {
